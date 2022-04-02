ff26b68f832a6c3dd8e6832d1793f401d287a2ae
Alex2077 <alexander.abarca@gmx.com>
2022-04-01 21:34:15 -0300

    Removed useless code

--- src/main/groovy/com/alex2077/rundeck/plugin/GoogleDriveNotificationPlugin.groovy
+++ src/main/groovy/com/alex2077/rundeck/plugin/GoogleDriveNotificationPlugin.groovy
@@ -30,10 +30,6 @@ class GoogleDriveNotificationPlugin implements NotificationPlugin {
     static final JsonFactory  JSON_FACTORY = GsonFactory.getDefaultInstance()
     static final List<String> SCOPES       = Collections.singletonList(DriveScopes.DRIVE)
 
-    static final String CREDENTIALS_FILE_PATH = "credentialsFilePath"
-    static final String FOLDER_ID             = "folderId"
-    static final String FILE_TO_UPLOAD        = "fileToUpload"
-
     @PluginProperty(
             title = "Credentials file path",
             description = "Credentials file path.",
@@ -55,41 +51,13 @@ class GoogleDriveNotificationPlugin implements NotificationPlugin {
     )
     String fileToUpload
 
-    /*static Description DESCRIPTION = DescriptionBuilder.builder()
-            .name(SERVICE_NAME)
-            .title(SERVICE_TITLE)
-            .description(SERVICE_DESCRIPTION)
-            .property(PropertyBuilder.builder()
-                    .string(CREDENTIALS_FILE_PATH)
-                    .title("Credentials file path")
-                    .description("Credentials file path.")
-                    .required(true)
-                    .build())
-            .property(PropertyBuilder.builder()
-                    .string(FOLDER_ID)
-                    .title("Folder ID")
-                    .description("Folder ID on Google Drive.")
-                    .required(true)
-                    .build())
-            .property(PropertyBuilder.builder()
-                    .string(FILE_TO_UPLOAD)
-                    .title("File to upload")
-                    .description("File to be uploaded to Google Drive.")
-                    .required(true)
-                    .build())
-            .build()*/
-
     @Override
     boolean postNotification(String trigger, Map executionData, Map config) {
-
-        //String credentialsFilePath = config.containsKey(CREDENTIALS_FILE_PATH) ? config.get(CREDENTIALS_FILE_PATH).toString() : null
-        //String folderId = config.containsKey(FOLDER_ID) ? config.get(FOLDER_ID).toString() : null
-        //String fileToUpload = config.containsKey(FILE_TO_UPLOAD) ? config.get(FILE_TO_UPLOAD).toString() : null
 
         boolean res = false
 
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport()
-        Credential credentials = getCredentials(HTTP_TRANSPORT, credentialsFilePath)
+        Credential credentials = getCredentials(credentialsFilePath)
         Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credentials)
                 .setApplicationName(SERVICE_NAME)
                 .build()
@@ -113,7 +81,7 @@ class GoogleDriveNotificationPlugin implements NotificationPlugin {
         return res
     }
 
-    private Credential getCredentials(final NetHttpTransport httpTransport, String credentialsPath) throws IOException  {
+    private Credential getCredentials(final String credentialsPath) throws IOException  {
 
         InputStream is = new FileInputStream(new java.io.File(credentialsPath))
         if (!is) {
